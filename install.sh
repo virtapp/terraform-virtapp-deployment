9bbfdf662a48880412829ff8e8b8bfc72ece6abc
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2023-07-24 23:45:06 +0300

    Initial commit

--- install.sh
+++ install.sh
@@ -34,7 +34,6 @@ sleep 10 && kubectl get pods -A && sleep 5
 helm repo add bitnami https://charts.bitnami.com/bitnami || true
 helm repo add hashicorp https://helm.releases.hashicorp.com|| true
 helm repo add rancher-latest https://releases.rancher.com/server-charts/latest || true
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts || true
 helm repo add secrets-store-csi-driver https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts || true
 helm repo add kedacore https://kedacore.github.io/charts || true
 helm repo update && sleep 5
@@ -46,7 +45,7 @@ helm install rancher rancher-latest/rancher --version=v2.7.0 \
   --set hostname=app-console.virtapp.io \
   --set ingress.tls.source=virtapp \
   --set replicas=1 \
-  --set bootstrapPassword="YWRtaW4="
+  --set bootstrapPassword="cTF3MmUzcjQxMDBA"
 kubectl create namespace keda || true
 helm install keda kedacore/keda --namespace keda --version "2.9.4" && sleep 5
 echo    Waiting for all pods in running mode:
@@ -72,7 +71,6 @@ printf "\nYou should see 'dashboard' as a reponse below (if you do the ingress i
 sleep 5 &&
 kubectl apply -f ./${path_folder}/ingress-app.yaml || true
 kubectl apply -f ./${path_folder}/ingress-argocd.yaml   || true
-kubectl apply -f ./${path_folder}/ingress-jenkins.yaml  || true
 sleep 5 && kubectl get nodes -o wide && sleep 5
 terraform providers && kubectl get ing -A
 
